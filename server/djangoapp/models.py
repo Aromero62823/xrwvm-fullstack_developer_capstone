e5e80659bf092f25d1d6498777f188c495207b5e
Aromero62823 <62823aromero4@gmail.com>
2026-07-16 22:18:44 -0400

    Fixed coding errors

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,8 +1,8 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
@@ -10,6 +10,7 @@ class CarMake(models.Model):
 
     def __str__(self):
         return self.name
+
 
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
@@ -20,8 +21,8 @@ class CarModel(models.Model):
         ('WAGON', 'Wagon')
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    
-    year = models.IntegerField(default=2026,
+    year = models.IntegerField(
+        default=2026,
         validators=[
             MaxValueValidator(2026),
             MinValueValidator(2015)
--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -5,21 +5,26 @@ from dotenv import load_dotenv
 load_dotenv()
 
 backend_url = os.getenv('backend_url', default="http://localhost:3030")
-sentiment_analyzer_url = os.getenv('sentiment_analyzer_url', default="http://localhost:5050/")
+sentiment_analyzer_url = os.getenv(
+    'sentiment_analyzer_url',
+    default="http://localhost:5050/"
+)
+
 
 def get_request(endpoint, **kwargs):
     params = ""
     if (kwargs):
-        for key, val in kwags.items():
-            params=params+key+'='+val+'&'
+        for key, val in kwargs.items():
+            params = params+key+'='+val+'&'
     request_url = backend_url+endpoint+'?'+params
 
     print('GET from {}'.format(request_url))
     try:
         response = requests.get(request_url)
         return response.json()
-    except:
-        print('Network exception occurred')
+    except Exception as err:
+        print(f'Error: {err}')
+
 
 def analyze_review_sentiments(text):
     request_url = sentiment_analyzer_url+"analyze/"+text
@@ -37,5 +42,5 @@ def post_review(data_dict):
         response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
-    except:
-        print("Netowkr exception occurred")
+    except Exception as err:
+        print(f'Error: {err}')
